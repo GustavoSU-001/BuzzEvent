a7b991135295a5f04c8b94140d953499e81b600d
GustavoSU-001 <uribeadolfo24@gmail.com>
2025-12-07 23:14:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from Interfaces.Master import BuzzEvent
+# Coloca esto al principio de main.py
+import kivy
+from kivy.config import Config
+Config.set('kivy', 'copy_images', '0') # Evita que Kivy intente copiar sus íconos
+Config.set('kivy', 'log_dir', 'logs') # Asegura un directorio de logs local.
+
+kivy.require('2.3.1') # Asegura la versión de Kivy
 
 BuzzEvent().run()
 
